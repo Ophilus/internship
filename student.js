f60fd6a1b55e1047cc2a8a181007acadcd78e799
Ophilus <gryn.vadim@gmail.com>
2019-03-24 23:58:39 +0200

    add array with data
    Array[  [ name ] , [ knowledge ] , [ university ] ]

--- student.js
+++ student.js
@@ -1,3 +1,24 @@
+var StudentList = [
+
+["Andrew Kostenko",
+		"Julia Veselkina",
+		"Maria Perechrest",
+		"Vasul Horban",
+		"Ihor Kostuk",
+		"Oleg Storozhuk",
+		"Romania Burko"],
+
+["4","5","3","4","2","1","5"],
+
+["Ivan Franko National University of Lviv",
+		"Lviv Polytechnic National University",
+		"Ukrainian Catholic University",
+		"Ivan Franko National University of Lviv",
+		"Lviv Polytechnic National University",
+		"Ukrainian Catholic University",
+		"Ivan Franko National University of Lviv"]
+
+];
 
 class Knowledge{
   constructor(level) {
